4e1a7aaac2d181398a1acfce6e8b91cdad23d219
FrancescoMussi <francesco.mussi@hotmail.it>
2020-06-07 20:06:17 +0300

    Update ContractView.vue

--- src/views/ContractView.vue
+++ src/views/ContractView.vue
@@ -95,32 +95,12 @@
 
     </b-card>
 
-
-    <b-modal ref="modalAssignProperties" class="modal-info">
-      <template slot="modal-header">
-        <h5 class="modal-title"><span v-lang.assignProperties></span></h5>
-      </template>
-      <span v-lang.assignPropertiesInfo></span>
-
-      <v-select multiple v-model="propertiesSelected" :options="propertiesFormatted"></v-select>
-
-      <template slot="modal-footer">
-        <b-btn variant="default" @click="$refs.modalAssignProperties.hide()">
-          <span v-lang.close></span>
-        </b-btn>
-        <b-btn variant="success" class="white" @click="updateProperties">
-          <span v-lang.update></span>
-        </b-btn>
-      </template>
-    </b-modal>
-
   </div>
 </template>
 
 <script>
   import store from '../vuex/store'
   import vSelect from 'vue-select'
-  import { savePropertiesInStorage } from '../mixins/savePropertiesInStorage'
   import { saveContractsInStorage } from '../mixins/saveContractsInStorage'
   import { createContractPdf } from '../mixins/createContractPdf'
   import pdfMake from 'pdfmake/build/pdfmake'
@@ -129,12 +109,9 @@
 
   export default {
     components: { vSelect },
-    mixins: [savePropertiesInStorage, saveContractsInStorage, createContractPdf],
+    mixins: [saveContractsInStorage, createContractPdf],
     data() {
       return {
-        propertiesSelected: [],
-        propertiesFormatted: [],
-        contractPropertiesScoped: [],
       }
     },
     computed: {
@@ -147,9 +124,6 @@
       currentContract() {
         return store.getters.getCurrentContract
       },
-      properties() {
-        return store.getters.getProperties
-      },
       user() {
         return store.getters.getUser
       },
@@ -158,26 +132,9 @@
     created() {
       this.$language = this.language
       console.log('currentContract', this.currentContract)
-      this.contractPropertiesScoped = this.currentContract.properties.map(item => ({...item}))
-      this.initPropertiesForModal()
     },
 
     methods: {
-      initPropertiesForModal() {
-        // Format properties list
-        for (let i = 0; i < this.properties.length; i++) {
-          let property = {}
-          property.value = this.properties[i].id
-          property.label = this.properties[i].name
-          this.propertiesFormatted.push(property)
-          // Create array with selected properties for the select
-          for (let j = 0; j < this.contractPropertiesScoped.length; j++) {
-            if (property.value === this.contractPropertiesScoped[j].id) {
-              this.propertiesSelected.push(property)
-            }
-          }
-        }
-      },
       goToContractEdit() {
         this.$localStorage.set('currentContract', JSON.stringify(this.currentContract))
         store.dispatch({
@@ -190,80 +147,6 @@
           contractFormType: 'edit'
         })
         this.$router.push({name: 'contractForm'})
-      },
-      openPropertiesModal() {
-        this.$refs.modalAssignProperties.show()
-      },
-      updateProperties() {
-        this.$refs.modalAssignProperties.hide()
-        let propertiesIds = []
-        // Reset properties in the view
-        this.contractPropertiesScoped = []
-        for (let i = 0; i < this.propertiesSelected.length; i++) {
-          propertiesIds.push(this.propertiesSelected[i].value)
-          // Updating in the view
-          let property = {}
-          property.name = this.propertiesSelected[i].label
-          property.id = this.propertiesSelected[i].value
-          this.contractPropertiesScoped.push(property)
-        }
-        let formData = new FormData()
-        formData.append('user_id', this.user.id)
-        formData.append('contract_id', this.currentContract.id)
-        formData.append('properties', JSON.stringify(propertiesIds))
-        this.$http.post(this.appApiPath + '/api/contract_properties_save', formData)
-          .then(response => {
-            console.log('success response from contract_properties_save')
-            console.log(response.body)
-            console.log('tha properties contractrs')
-            console.log(this.contractPropertiesScoped)
-
-            // Update current contract
-            let contractUpdated = this.currentContract
-            contractUpdated.properties = this.contractPropertiesScoped
-            store.dispatch({
-              type: 'setAppCurrentContract',
-              currentContract: contractUpdated
-            })
-            this.$localStorage.set('currentContract', JSON.stringify(contractUpdated))
-
-            // Update contracts
-            store.dispatch({
-              type: 'setAppContracts',
-              contracts: response.body.data.contracts
-            })
-            this.saveContractsInStorage(response.body.data.contracts)
-
-            // Update properties
-            store.dispatch({
-              type: 'setAppProperties',
-              properties: response.body.data.properties
-            })
-            this.savePropertiesInStorage(response.body.data.properties, 'admin')
-
-            // Display toast message
-            if (this.contractPropertiesScoped.length > 0) {
-              if (this.$language === 'en') {
-                this.$toasted.show('Contract successfully assigned to the property!', {icon: 'fa-check-circle', type: 'success'})
-              } else if (this.$language === 'tr') {
-                this.$toasted.show('Sözleşme Salon ile eşleştirildi!', {icon: 'fa-check-circle', type: 'success'})
-              }
-            } else if (this.contractPropertiesScoped.length === 0) {
-              if (this.$language === 'en') {
-                this.$toasted.show('Contract assignments successfully updated.', {icon: 'fa-check-circle', type: 'success'})
-              } else if (this.$language === 'tr') {
-                this.$toasted.show('Eşleştirme düzenleme işlemi tamamlandı.', {icon: 'fa-check-circle', type: 'success'})
-              }
-            }
-          }, response => {
-            console.warn('error from contract_properties_save')
-            console.log(response)
-            if (this.$language === 'en') {
-              this.$toasted.show('Something went wrong', {icon: 'fa-exclamation-triangle', type: 'error'})
-            } else if (this.$language === 'tr') {
-              this.$toasted.show('Bir hata oluştu', {icon: 'fa-exclamation-triangle', type: 'error'})
-            }
-          })
       },
       makePreview(event) {
         event.preventDefault()
